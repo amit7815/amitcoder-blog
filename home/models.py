637cd0c69f3f39a2591bcd12ad6bf32948471443
Amit Yadav <yadavamit14801@gmail.com>
2020-09-28 17:09:35 +0530

    lecture78

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# Database -----> Excel workbook
+# Models in Django ----> Table------>Excel Sheet
+
+class Contact(models.Model):
+    sno=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=250)
+    phone=models.CharField(max_length=15)
+    email=models.CharField(max_length=100)
+    content=models.TextField()
+    timestamp=models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return "message from---"+ self.name +"---->"+self.email
+    
+
+
+ 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def contact(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content=request.POST['content']
+        #print(name,email,phone,content)
+        contact=Contact(name=name,phone=phone,email=email,content=content)
+        #for saving in database
+        contact.save()
     return render(request,'home/contact.html')
 
 def home(request):
