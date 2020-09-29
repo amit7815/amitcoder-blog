be7944224a2b96fe3015a2479359ac372524f1b4
Amit Yadav <yadavamit14801@gmail.com>
2020-09-29 14:26:57 +0530

    video81

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    sno=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=250)
+    content=models.TextField()
+    author=models.CharField(max_length=40)
+    slug=models.CharField(max_length=150)
+    timestamp=models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title +" by "+self.author
